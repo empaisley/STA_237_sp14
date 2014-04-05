4953bb08b2317f663cfbddf69d017111c6c1719a
empaisley <empaisley@ucdavis.edu>
2014-04-04 21:09:16 -0700

    updated R code

--- Homework/237_hw1.R
+++ Homework/237_hw1.R
@@ -1,26 +1,48 @@
-library(matrixcalc)
-
-pop <- read.table("~/School/UC Davis/sta 237 - s14/population.csv", quote="\"")
-y = as.vector(pop)
-
-a = as.vector(seq(1790,2000,10))
+rm(list=ls(all=TRUE))
+setwd("C:/Users/EliotP/Documents/GitHub/STA_237_sp14/Homework/") #laptop
+pop = as.matrix(read.table("population.csv"))
 n = nrow(pop)
 
-plot(1:n,y=t(pop))
 
+### plot the data
+time = as.vector(seq(1790,2000,10))
+pdf("hw1_dataplot.pdf")
+plot(time,t(pop),type="o",xlab="Year", ylab="Population",main="U.S. Population in 10-Year Increments")
+dev.off()
+
+library(matrixcalc)
+
+### fit a 3rd degree polynomial
 F = vandermonde.matrix(1:n,3)
-mhat_ls = solve(t(F)%*%F)%*%t(F)%*%pop 
+mhat_ls = F%*%solve(t(F)%*%F)%*%t(F)%*%pop 
 
-dat = as.data.frame(cbind(y,F))
+library(xtable)
+print(xtable(mhat_ls),include.rownames=FALSE)
+
+## alternate method to comuting least-squares fit
+dat = as.data.frame(cbind(pop,F))
 names(dat) = c("y","one","two","three")
-model = lm(y~.,data=dat)
+model = lm(y~0+.,data=dat)
 model$fit
 
-plot(1:n, model$fit,type="l")
-points(pop)
+### residuals
 
+resid = pop-mhat_ls
+pdf("hw1_resid.pdf")
+plot(x=time, resid,type="o",xlab="Time",ylab="Residual",main="Plot of Residuals against Time")
+abline(h=0)
+dev.off()
+### predict using direct method
+A=solve(t(F)%*%F)%*%t(F)%*%pop
+predict_val = as.matrix(c(A[1]*1+A[2]*23+A[3]*23^2,A[1]*1+A[2]*24+A[3]*24^2))
+print(xtable(t(predict_val)),include.rownames=FALSE)
 
-
+###predict using lm function
 new = c(23,24)
 new_df = data.frame(one = 1, two=new, three = new^2)
-predict.lm(model,new_df)
+B = predict.lm(model,new_df)
+
+
+#plot(model$fit,xlim=c(0,25),ylim=c(0,max(B)))
+#points(pop,type="o")
+#points(new,B)
